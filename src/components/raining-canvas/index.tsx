98470b2e176f6122ef5d9fc764558262524d38a4
Byongho96 <unlike96@gmail.com>
2023-07-06 23:57:37 +0900

    update 404 page

--- src/components/raining-canvas/index.tsx
+++ src/components/raining-canvas/index.tsx
@@ -10,8 +10,8 @@ type Props = {
 }
 
 const RainingCanvas: React.FC<Props> = ({
-  amount = 40,
-  speed = 20,
+  amount = 30,
+  speed = 15,
   color = '#ffffff',
   isThunder = true,
   thunderRate = 0.1,
@@ -20,21 +20,43 @@ const RainingCanvas: React.FC<Props> = ({
   const canvasRef = useRef<HTMLCanvasElement>(null)
 
   useEffect(() => {
+    // amount 제한 1 ~ 100
+    if (amount <= 0 || 100 < amount) {
+      throw new Error(
+        'amount is out of range, It should be greater than 0 and less than 100'
+      )
+    }
+    // speed 제한 1 ~ 50
+    if (speed <= 0 || 50 < speed) {
+      throw new Error(
+        'speed is out of range, It should be greater than 0 and less than 50'
+      )
+    }
+    // thunderRate 제한 0~1
+    if (thunderRate <= 0 || 1 < thunderRate) {
+      throw new Error(
+        'thunderRate is out of range, It should be greater than 0 and less than 1'
+      )
+    }
+  }, [amount, speed, thunderRate])
+
+  useEffect(() => {
     const canvas = canvasRef.current
     const ctx = canvas.getContext('2d')
-    let rainDropArray = []
-    let rainSplashArray = []
+    const mouse = { x: 0, y: 0, isActive: false } // 마우스 정보
+    let rainDropArray = [] // 빗줄기 인스턴스를 담는 배열
+    let rainSplashArray = [] // 튀기는 빗방울 인스턴스름 담는 배열
     let thunder = null
     if (isThunder) {
       thunder = new Thunder()
     }
 
-    const mouse = { x: 0, y: 0, isActive: false } // 마우스 정보
-
     // canvas 초기화 함수
-    const init = () => {
+    function init() {
       rainDropArray = []
-      rainSplashArray = [] // 이거는 RainDrop 클래스에서 채워진다
+      rainSplashArray = []
+
+      // containerRef에 따라 캔버스 크기 조절
       const innerWidth = containerRef
         ? containerRef.current.offsetWidth
         : window.innerWidth
@@ -43,50 +65,75 @@ const RainingCanvas: React.FC<Props> = ({
         : window.innerHeight
       canvas.width = innerWidth
       canvas.height = innerHeight
-      const total = Math.floor((innerWidth * innerHeight * amount) / 400000)
 
+      // 캔버스 크기에 따라 빗줄기 갯수 설정
+      const total = Math.floor((innerWidth * innerHeight * amount) / 400000)
+      // 빗줄기 생성
       for (let i = 0; i < total; i++) {
         const x = Math.random() * innerWidth
         const y = Math.random() * innerHeight
-        // const dx = Math.random() * 2 - 1 // -1 ~ 1 // 속도에 따라 달라지므로 animate 함수에서 계산
-        const dy = Math.random() * 5 + speed // 13 ~ 18
-        rainDropArray.push(new RainDrop(x, y, dy, color, rainSplashArray))
+        const dx = Math.random() * 0.1 - 0.05
+        const dy = Math.random() * 5 + parseInt(speed) // 왜인지 오류가 남
+        console.log(speed, dy, innerHeight, y)
+        rainDropArray.push(new RainDrop(x, y, dx, dy, color, rainSplashArray))
       }
+
+      console.log(rainDropArray)
     }
 
     // cavnas 렌더링(애니메이션) 함수
-    const render = () => {
+    let requestAnimationId = null
+    function render() {
       ctx.clearRect(0, 0, canvas.width, canvas.height)
-      rainDropArray.forEach((rainDrop) => rainDrop.animate(ctx, mouse))
-      rainSplashArray.forEach((rainSplash, index) => {
+      // 빗줄기 애니메이션
+      const wind = mouse.isActive ? (mouse.x - ctx.canvas.width / 2) / 80 : 0 // 마우스 위치에 따른 바람
+      rainDropArray.forEach((rainDrop) => {
+        rainDrop.animate(ctx, wind)
+      })
+      // 튀기는 빗방울 애니메이션
+      rainSplashArray.forEach((rainSplash, idx) => {
         rainSplash.animate(ctx)
-        // 튀기는 물방울은 1회성으로 제거
         if (rainSplash.y > canvas.height) {
-          rainSplashArray.splice(index, 1)
+          rainSplashArray.splice(idx, 1) // 튀기는 물방울은 1회성으로 제거
         }
       })
+      // 천둥 애니메이션
       if (thunder) {
         if (Math.random() < thunderRate * 0.05) {
           thunder.opacity = 0.3
         }
         thunder.animate(ctx)
       }
-      requestAnimationFrame(render)
+      requestAnimationId = window.requestAnimationFrame(render) // requestAnmiationFrame
     }
 
-    window.addEventListener('resize', init)
-    canvas.addEventListener('mouseenter', () => (mouse.isActive = true))
-    canvas.addEventListener('mouseleave', () => (mouse.isActive = false))
-    canvas.addEventListener('mousemove', (e) => {
-      mouse.x = e.clientX // 마우스 좌표를 넣어줌
-      mouse.y = e.clientY
-    })
+    init() // 초기화
+    render() // 애니메이션 시작
 
-    init()
-    render()
+    // 화면 리사이즈 시, 초기화 함수 동작
+    window.addEventListener('resize', init)
+
+    // 캔버스에 마우스 관련 이벤트리스너 등록 => mouse 객체 업데이트
+    function setMouseActive() {
+      mouse.isActive = true
+    }
+    function setMouseInactive() {
+      mouse.isActive = false
+    }
+    function setMousePosition(e) {
+      mouse.x = e.clientX
+      mouse.y = e.clientY
+    }
+    canvas.addEventListener('mouseenter', setMouseActive)
+    canvas.addEventListener('mouseleave', setMouseInactive)
+    canvas.addEventListener('mousemove', setMousePosition)
 
     return () => {
+      window.cancelAnimationFrame(requestAnimationId)
       window.removeEventListener('resize', init)
+      canvas.removeEventListener('mouseenter', setMouseActive)
+      canvas.removeEventListener('mouseleave', setMouseInactive)
+      canvas.removeEventListener('mousemove', setMousePosition)
     }
   }, [color, amount, speed, isThunder, thunderRate, containerRef])
 
@@ -98,46 +145,47 @@ export default RainingCanvas
 // 떨어지는 빗방울
 class RainDrop {
   // 생성자 (위치 좌표, 이동 속도)
-  constructor(x, y, dy, color, splashArray) {
+  constructor(x, y, dx, dy, color, splashArray) {
     this.x = x
     this.y = y
+    this.dx = dx
     this.dy = dy
+    this.wind = 0
     this.color = color
     this.splashArray = splashArray
   }
 
   // 빗줄기 그리기
   draw(ctx) {
-    const { x, y, dx, dy, color } = this
+    const { x, y, dx, dy, wind, color } = this
     ctx.beginPath()
     ctx.moveTo(x, y)
-    ctx.lineTo(x + 2 * dx, y + 2 * dy)
+    ctx.lineTo(x + dx + wind, y + 1.5 * dy)
     ctx.strokeStyle = color
     ctx.lineWidth = 1
     ctx.stroke()
   }
 
-  // 튀기는 물방울 그리기
+  // 튀기는 물방울 생성
   splash() {
     const { x, y, dx, dy, color, splashArray } = this
     for (let i = 0; i < 3; i++) {
-      const splash_dx = -dx + Math.random() * 6 - 3
-      const splash_dy = -dy + Math.random() * 3 + 11
+      const splash_dx = Math.random() * 6 + dx - 1 // 물방울의 x축 속도
+      const splash_dy = Math.random() * 0.7 - dy * 0.24 // 물방울의 y축 속도
       splashArray.push(new RainSplash(x, y, splash_dx, splash_dy, color))
     }
   }
 
   // 빗줄기 이동
-  animate(ctx, mouse) {
+  animate(ctx, wind = 0) {
+    // 빗줄기가 캔버스 아래로 내려가면, 다시 위로 이동
     if (this.y > ctx.canvas.height) {
       this.splash()
-      this.x = Math.random() * ctx.canvas.width * 1.4 - ctx.canvas.width * 0.2 // 아니면 시간이 지날 수록, 언젠가는 빗줄기가 캔버스 바깥으로 사라진다
-      this.y = -20
+      this.x = Math.random() * ctx.canvas.width * 1.4 - ctx.canvas.width * 0.2 // 적당히 캔버스의 너비를 초과하게
+      this.y = -50 // 적당히 캔버스 위
     }
-    this.dx = mouse.isActive
-      ? Math.random() * 2 - 1 + (mouse.x - ctx.canvas.width / 2) / 80
-      : Math.random() * 2 - 1
-    this.x += this.dx
+    this.wind = wind
+    this.x += this.dx + wind
     this.y += this.dy
     this.draw(ctx)
   }
@@ -152,23 +200,24 @@ class RainSplash {
     this.dx = dx
     this.dy = dy
     this.color = color
-    this.gravity = 1.5
+    this.GRAVITY = 1.5 // MAGIC_NUMBER: 중력가속도
   }
 
-  // 물방울 그리기
+  // 물방울(동그라미) 그리기
   draw(ctx) {
     const { x, y, color } = this
+    const radius = Math.random() * 0.5 + 0.85
     ctx.beginPath()
-    ctx.arc(x, y, 1.5, 0, Math.PI * 2) // 원형
+    ctx.arc(x, y, radius, 0, Math.PI * 2) // 원 그리기 arc(x, y, radius, startAngle, endAngle)
     ctx.fillStyle = color
     ctx.fill()
   }
 
   // 물방울 애니메이션 (포물선 형태로 올라갔다가 떨어짐)
   animate(ctx) {
-    this.dy += this.gravity
     this.x += this.dx
     this.y += this.dy
+    this.dy += this.GRAVITY // 중력 가속도
     this.draw(ctx)
   }
 }
@@ -181,7 +230,7 @@ class Thunder {
 
   // 천둥 그리기
   draw(ctx) {
-    const gradient = ctx.createLinearGradient(0, 0, 0, ctx.canvas.height) // gradient 좌표정보
+    const gradient = ctx.createLinearGradient(0, 0, 0, ctx.canvas.height) // gradient 좌표정보 (시작 x, 시작 y, 끝 x, 끝 y)
     gradient.addColorStop(0, `rgba(180, 190, 255, ${this.opacity})`) // 시작 색상 (offset, color)
     gradient.addColorStop(1, `rgba(0, 0, 0, 0)`) // 끝 색상(offset, color)
     ctx.fillStyle = gradient
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,29 +1,79 @@
-import React, { useContext, useRef } from 'react'
+import React, { useContext, useRef, useState, useMemo } from 'react'
 import './404.scss'
+import rainAudio from '@assets/audio/rain-audio.wav'
 import RainingCanvas from '@components/raining-canvas'
 import SEO from '@components/seo'
 import { ThemeContext } from '@contexts/theme-context'
 
 const NotFoundPage: React.FC<PageProps> = () => {
-  const containerRef = useRef<HTMLDivElement>(null)
   const { theme } = useContext(ThemeContext)
+  const containerRef = useRef<HTMLDivElement>(null)
 
-  let color = '#ffffff'
-  let isThunder = true
-  if (theme === 'light') {
-    color = '#888888'
-    isThunder = false
-  }
+  const [amount, setAmount] = useState(30)
+  const [speed, setSpeed] = useState(15)
+  const [thunderRate, setThunderRate] = useState(0.1)
+
+  console.log(speed)
+
+  const { color, isThunder } = useMemo(() => {
+    let color = '#eeeeee'
+    let isThunder = true
+    if (theme === 'light') {
+      color = '#888888'
+      isThunder = false
+    }
+    return { color, isThunder }
+  }, [theme])
 
   return (
     <div className="not-found--layout" ref={containerRef}>
-      <div className="not-found__text">404</div>
-      <div className="not-found__background">
-        <RainingCanvas
-          containerRef={containerRef}
-          color={color}
-          isThunder={isThunder}
-        />
+      <RainingCanvas
+        containerRef={containerRef}
+        color={color}
+        isThunder={isThunder}
+        amount={amount}
+        speed={speed}
+        thunderRate={thunderRate}
+      />
+      <div className="not-found__content">
+        <audio src={rainAudio} autoplay loop />
+        <div className="not-found__text">404</div>
+        <div className="not-found__input">
+          <label htmlFor="rain-amount-input">Rain Amount</label>
+          <input
+            type="range"
+            id="rain-amount-input"
+            name="rain-amount"
+            min="1"
+            max="100"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+        </div>
+        <div className="not-found__input">
+          <label htmlFor="rain-speed-input">Rain Speed</label>
+          <input
+            type="range"
+            id="rain-speed-input"
+            name="rain-speed"
+            min="1"
+            max="50"
+            value={speed}
+            onChange={(e) => setSpeed(e.target.value)}
+          />
+        </div>
+        <div className="not-found__input">
+          <label htmlFor="rain-amount-input">Thunder Rate</label>
+          <input
+            type="range"
+            id="thunder-rate-input"
+            name="thunder-rate"
+            min="1"
+            max="10"
+            value={thunderRate * 10}
+            onChange={(e) => setThunderRate(e.target.value / 10)}
+          />
+        </div>
       </div>
     </div>
   )
